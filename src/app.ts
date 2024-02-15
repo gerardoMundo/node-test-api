c0390c82b9b86ed1ba78d07860137f88f6b0330b
Gerardo Mundo <ger.mundo66@gmail.com>
2024-02-15 13:24:10 -0600

    Initial test routes added

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,10 @@
 const express = require('express');
+import routes from './routes/test.routes';
 
 const app = express();
 
 app.set('port', 3001);
 app.use(express.json());
+app.use(routes);
+
 export default app;
